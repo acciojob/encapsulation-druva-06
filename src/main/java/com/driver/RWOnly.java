485e9dba7ddb8b29690de7a467b0b7737b4039f8
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-03-19 21:54:54 +0530

    Encap Assignment

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    public String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
